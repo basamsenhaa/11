a3f98f7850398a16eec0c388cdaeccb3985326fa
rumbahumba <basamsenha@gmail.com>
2025-11-14 17:06:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,11 +4,11 @@ API_ID = os.environ.get("API_ID", "26375665")
 
 API_HASH = os.environ.get("API_HASH", "568839157ce65f4d3a91647f022b6737")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8380810929:AAEltCJe5zg4SE0lYav3NcSx02pTU8wMsC8")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8390366595:AAFnbW6VyXqY6_O_2zLf5CxpID_GaY7T8B4")
 
 PASS_DB = int(os.environ.get("PASS_DB", "720"))
 
-#OWNER = int(os.environ.get("OWNER", "6834250190"))
+#OWNER = int(os.environ.get("OWNER", "7547625729"))
 
 #LOG = ,
 
@@ -17,8 +17,8 @@ PASS_DB = int(os.environ.get("PASS_DB", "720"))
 # auth_chats = []
 
 #try:
-   #ADMINS=[6834250190]
-    #for x in (os.environ.get("ADMINS", "6834250190").split()):
+   #ADMINS=[]
+    #for x in (os.environ.get("ADMINS", "").split()):
         #ADMINS.append(int(x))
 #except ValueError:
         #raise Exception("Your Admins list does not contain valid integers.")
