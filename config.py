8d42b78967d6fefe79b54d84a003779cb198cca7
kabaci1299 <basamsenha@gmail.com>
2025-11-27 15:03:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ API_ID = os.environ.get("API_ID", "26375665")
 
 API_HASH = os.environ.get("API_HASH", "568839157ce65f4d3a91647f022b6737")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8500983480:AAHJT1uBIgHTCKar0qOOfMSF2hyk3gxC35U")
 
 PASS_DB = int(os.environ.get("PASS_DB", "720"))
 
